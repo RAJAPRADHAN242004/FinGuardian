d360634159b509d9fa64cafbbf16876db164ed62
RAJA DEMO <pradhanraja780@gmail.com>
2025-08-02 22:33:07 +0530

    set up commit

---
+++ finguardian/src/main/java/in/moneymanager/finguardian/FinguardianApplication.java
@@ -0,0 +1,13 @@
+package in.moneymanager.finguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinguardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinguardianApplication.class, args);
+	}
+
+}
---
+++ finguardian/src/test/java/in/moneymanager/finguardian/FinguardianApplicationTests.java
@@ -0,0 +1,13 @@
+package in.moneymanager.finguardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinguardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
